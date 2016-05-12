31b25736c54a07830e10bd56f6c010af187067f8
fedehipper <federicohipper@gmail.com>
2016-05-12 16:34:41 -0300

    modificado parametros variables

--- Biblioteca_Listas_C/src/main.c
+++ Biblioteca_Listas_C/src/main.c
@@ -2,16 +2,22 @@
 #include <stdlib.h>
 #include "estructuras.h"
 #include "listas.h"
+#include <stdarg.h>
+
+
+#define contenido_length 2
 
 struct t_contenido {
 	int un_valor;
 	char * un_string;
 };
 
-struct t_contenido * llenar(int valor, char * cadena);
+
+struct t_contenido * llenar(int longitud, ... );
 void mostrar(nodo ** una_lista);
 nodo * new(int valor, char * cadena);
 
+/*---------------------------------------------------------------------------------*/
 
 int main(void) {
 	nodo * uno = new(1, "hola");
@@ -27,33 +33,30 @@ int main(void) {
 	insertar_por_indice(&una_lista, &otro , 0);
 	insertar_por_indice(&una_lista, &tres, 1);
 
-
 	mostrar(&una_lista);
 
 	return EXIT_SUCCESS;
 }
 
-
-/*-----------------------------------------------------------------*/
+/*---------------------------------------------------------------------------------*/
 
 nodo * new(int valor, char * cadena) {
-	return crear_nodo(llenar(valor, cadena));
+	return crear_nodo(llenar(contenido_length, valor, cadena));
 }
 
-struct t_contenido * llenar(int valor, char * cadena) {
-	struct t_contenido * cont = malloc(sizeof(struct t_contenido));
-	cont->un_valor = valor;
-	cont->un_string = cadena;
-	return cont;
+struct t_contenido * llenar(int longitud, ... ) {
+	struct t_contenido * contenido = malloc(sizeof(struct t_contenido));
+	va_list args;
+	va_start(args, longitud);
+	contenido->un_valor = va_arg(args, int);
+	contenido->un_string = va_arg(args, char *);
+	return contenido;
 }
 
 void mostrar(nodo ** una_lista) {
-	nodo * aux;
-	aux = *una_lista;
+	nodo * aux = *una_lista;
 	while(aux != NULL) {
-		struct t_contenido * con;
-		con = aux->contenido;
-		printf("%d -----  %s\n", con->un_valor, con->un_string);
+		printf("%d -----  %s\n", aux->contenido->un_valor, aux->contenido->un_string);
 		aux = aux->sig;
 	}
 }
