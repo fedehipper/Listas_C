82e3a3ec3069b91aa53e38715db089f826756c86
fedehipper <federicohipper@gmail.com>
2016-05-13 04:19:53 -0300

    codigo de mas suprimido

--- Biblioteca_Listas_C/src/listas.c
+++ Biblioteca_Listas_C/src/listas.c
@@ -2,8 +2,6 @@
 #include <stdlib.h>
 #include "estructuras.h"
 
-
-
 nodo * crear_nodo(void * estructura) {
 	nodo * un_nodo = malloc(sizeof(nodo));
 	un_nodo->contenido = estructura;
@@ -13,8 +11,7 @@ nodo * crear_nodo(void * estructura) {
 
 /* retorna la cantidad de nodos de la lista */
 int size(nodo ** lista) {
-	nodo * aux = malloc(sizeof(nodo));
-	aux = *lista;
+	nodo * aux = *lista;
 	int longitud = 0;
 	while(aux != NULL) {
 		aux = aux->sig;
@@ -25,8 +22,7 @@ int size(nodo ** lista) {
 
 /* retorna el nodo de acuerdo con un indice que recibe por parametro */
 nodo * busca_por_indice(nodo ** lista, int indice) {
-	nodo * aux = malloc(sizeof(nodo));
-	aux = *lista;
+	nodo * aux = *lista;
 	int longitud = size(lista), i;
 	for(i = 0; i < longitud ; i++) {
 		if(i == indice) break;
@@ -38,8 +34,7 @@ nodo * busca_por_indice(nodo ** lista, int indice) {
 
 /* mete un nodo o elemento al final de la lista como cola */
 void encolar(nodo ** un_nodo, nodo ** lista) {
-	nodo * ultimo = malloc(sizeof(nodo));
-	ultimo = busca_por_indice(lista, size(lista) -1);
+	nodo * ultimo = busca_por_indice(lista, size(lista) -1);
 	(*un_nodo)->sig = NULL;
 	ultimo == NULL ? *lista = *un_nodo : (ultimo->sig = *un_nodo);
 }
@@ -47,8 +42,7 @@ void encolar(nodo ** un_nodo, nodo ** lista) {
 
 /* mete un nodo o elemento al principio de la lista como pila */
 void apilar(nodo ** un_nodo, nodo ** lista) {
-	nodo * aux = malloc(sizeof(nodo));
-	aux = *lista;
+	nodo * aux = *lista;
 	if (*lista == NULL) {
 		(*un_nodo)->sig = NULL;
 		*lista = *un_nodo;
@@ -62,9 +56,7 @@ void apilar(nodo ** un_nodo, nodo ** lista) {
 
 /* elimina el nodo de acuerdo al indice comenzando de 0 y si se paso de rango, no elimina */
 void eliminar(nodo ** lista, int indice) {
-	nodo * post = malloc(sizeof(nodo)), *ant = malloc(sizeof(nodo));
-	post = *lista;
-	ant = post;
+	nodo * post = *lista, *ant = *lista;
 	int count = 0;
 	while(post != NULL && indice > count) {
 		ant = post;
@@ -81,8 +73,7 @@ void eliminar(nodo ** lista, int indice) {
 
 /* elimina el primer elemento y lo retorna */
 nodo * sacar(nodo ** lista) {
-	nodo * aux = malloc(sizeof(nodo));
-	aux = *lista;
+	nodo * aux = *lista;
 	if(*lista != NULL) {
 		*lista = aux->sig;
 		return aux;
@@ -98,10 +89,8 @@ void insertar_por_indice(nodo ** lista, nodo ** elemento, int indice) {
 	else if(indice == size(lista))
 		encolar(elemento, lista);
 	else {
-		nodo *anterior = malloc(sizeof(nodo));
-		nodo * siguiente = malloc(sizeof(nodo));
-		anterior = busca_por_indice(lista, indice - 1);
-		siguiente = anterior->sig;
+		nodo * anterior = busca_por_indice(lista, indice - 1);
+		nodo * siguiente = anterior->sig;
 		anterior->sig = *elemento;
 		(*elemento)->sig = siguiente;
 	}
