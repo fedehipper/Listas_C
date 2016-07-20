293d892ed9234430d426e5c686b507f8abe2cecd
fedehipper <fedehipper@gmail.com>
2016-07-19 22:36:02 -0300

    refactor

--- Biblioteca_Listas_C/src/listas.c
+++ Biblioteca_Listas_C/src/listas.c
@@ -23,8 +23,7 @@ nodo * busca_por_indice(nodo ** lista, int indice) {
 	nodo * aux = *lista;
 	int i, length = size(lista);
 	for(i = 0 ; i < length ; i++) {
-		if(i == indice)
-			break;
+		if(i == indice) break;
 		aux = aux->sig;
 	}
 	return indice >= 0 && indice <= length ? aux : NULL;
@@ -59,8 +58,7 @@ void eliminar(nodo ** lista, int indice) {
 		count++;
 	}
 	if(indice >= 0 && post != NULL) {
-		if (count == 0)
-			*lista = ant->sig;
+		if (count == 0) *lista = ant->sig;
 		ant->sig = count == indice && count != 0 && post->sig == NULL ? NULL : post->sig;
 	}
 }
@@ -72,21 +70,18 @@ nodo * sacar(nodo ** lista) {
 		*lista = aux->sig;
 		return aux;
 	}
-	else
-		return NULL;
+	else return NULL;
 }
 
 void insertar_por_indice(nodo ** lista, nodo ** elemento, int indice) {
-	if(indice == 0)
-		apilar(elemento, lista);
-	else if(indice == size(lista))
-		encolar(elemento, lista);
-	else {
-		nodo * anterior = busca_por_indice(lista, indice - 1);
-		nodo * siguiente = anterior->sig;
-		anterior->sig = *elemento;
-		(*elemento)->sig = siguiente;
-	}
+	if(indice == 0) apilar(elemento, lista);
+	else if(indice == size(lista)) encolar(elemento, lista);
+		else {
+			nodo * anterior = busca_por_indice(lista, indice - 1);
+			nodo * siguiente = anterior->sig;
+			anterior->sig = *elemento;
+			(*elemento)->sig = siguiente;
+		}
 }
 
 void reverse(nodo ** lista) {
